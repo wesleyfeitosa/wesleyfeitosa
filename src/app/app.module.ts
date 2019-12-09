aa5fab8869325610a77dd32f67162c1d602b5b4a
wesleyfeitosa <jwesleydasilva@gmail.com>
2019-12-09 15:32:14 -0300

    implementando angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    /* --------- Componentes do Angular Material --------- */
+    MatToolbarModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule
+    /* --------- Fim do componentes Do Angular Material ---------- */
   ],
   providers: [],
   bootstrap: [AppComponent]
